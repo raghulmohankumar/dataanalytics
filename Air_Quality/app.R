59e5bef160f15468d07c88210b7b677da969c029
raghulmohankumar <83571194+raghulmohankumar@users.noreply.github.com>
2021-06-11 20:59:21 +0530

    A04 iter 3

---
+++ Air_Quality/app.R
@@ -0,0 +1,95 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+
+library(shiny)
+library(shinyanimate)
+library(shinybusy)
+library(shinyjs)
+library(tidyverse)
+library(tidytext)
+library(plotly)
+library(glue)
+library(rsconnect)
+library(shinycustomloader)
+library(aos)
+library(animation)
+
+
+## Import Estimated_Inpatient_Beds.csv
+
+Indian_air_quality_index <- read_csv(file = "air_quality_index_india.csv")
+
+## Transform Data
+
+# ui User Interface
+
+ui <- fluidPage(
+    align = "center",
+    titlePanel("Selection based Air Quality Index in Indian Cities"),
+    downloadButton("testgif"),
+    add_busy_spinner(spin = "fading-circle"),
+    sidebarLayout(
+        sidebarPanel(
+            selectInput(
+                inputId = "city",
+                label = "Select City",
+                choices = c("Bengaluru","Bhopal","Chandigarh","Chennai","Gandhinagar","Hyderabad","Jaipur","Kolkata","Lucknow","Mumbai","Mysore",
+                            "Nagpur","New Delhi","Patna","Shillong","Thiruvananthapuram","Thrissur","Visakhapatnam"
+                )
+            ),
+            dateRangeInput(
+                inputId = "date_range",
+                label = "Select date range",
+                start = min(Indian_air_quality_index$date),
+                end = max(Indian_air_quality_index$date)
+            )
+        ), 
+        mainPanel(
+            plotlyOutput(outputId = "Quality_plot")
+        )
+    )
+)
+
+# Server
+
+server <- function(input, output) {
+    output$Quality_plot <- renderPlotly({
+        p <- Indian_air_quality_index %>%
+            filter(city == input$city) %>%
+            filter(between(date, input$date_range[1], input$date_range[2])) %>%
+            count(date, value) %>%
+            ggplot(aes(x = date, y = value)) +
+            geom_line(color = "gray") +
+            scale_fill_discrete(name="AQ Index", labels = c("Poor Quality","Good Quality"))+
+            geom_point(aes(color = value < 100), size = 1) +
+            geom_hline(yintercept = 0, linetype = "dashed", color = "lightgray") +
+            labs(
+                title = glue("Pollution Index in Indian Cities, {input$origin} Air quality in Indian Cities"),
+                x = "Date", y = "Air Quality Index Score"
+            ) +
+            theme_minimal() +
+            theme(legend.position = "none")
+        
+        ggplotly(p, tooltip = c("x", "y"))
+    })
+    
+    output$testgif = downloadHandler(
+        filename = 'random.gif',
+        content  = function(file) {
+            saveGIF(
+                for (i in 1:30) {
+                    plot(rnorm(100))
+                }, movie.name = 'random.gif', interval = 0.1)
+            file.rename('random.gif', file)
+        })
+    }
+    
+# Shiny App
+shinyApp(ui = ui, server = server)
